3cf19d0e3351307d23d778e03c3df2e4d0d0157c
294423 <riko64222@gmail.com>
2025-08-16 21:14:56 +0700

    Update ImageData.kt

--- app/src/main/java/app/hyperpic/data/ImageData.kt
+++ app/src/main/java/app/hyperpic/data/ImageData.kt
@@ -1,18 +1,21 @@
 package app.hyperpic.data
 
 import android.net.Uri
+import android.graphics.Bitmap
 
 /**
  * A data class to hold information about a single image.
  *
- * @property uri The content Uri for the image, which is used for display.
+ * @property id The MediaStore ID for the image (from MediaStore.Images.Media._ID).
+ * @property uri The content Uri for the image, which is used to display it.
  * @property name The display name of the image file.
- * @property path The absolute path of the image file.
- * @property cacheUri An optional Uri for a cached version of the image.
+ * @property path The absolute path of the image file on the device.
+ * @property thumbnail Optional cached thumbnail bitmap.
  */
 data class ImageData(
+    val id: Long,
     val uri: Uri,
     val name: String,
     val path: String,
-    val cacheUri: Uri? = null
+    val thumbnail: Bitmap? = null
 )
