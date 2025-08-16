9677762df06c4434ff6d0a335add63350c340af7
294423 <riko64222@gmail.com>
2025-08-16 21:15:22 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "app.hyperpic"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 28
         versionCode = 1
         versionName = "1.0"
         androidResources {
