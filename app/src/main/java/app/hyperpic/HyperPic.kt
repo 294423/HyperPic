d6715e1d8b4279d4b2a56a6efeea2fb369c23ccb
294423 <riko64222@gmail.com>
2025-08-16 21:13:11 +0700

    Update HyperPic.kt

--- app/src/main/java/app/hyperpic/HyperPic.kt
+++ app/src/main/java/app/hyperpic/HyperPic.kt
@@ -6,9 +6,9 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,85 +18,110 @@ import java.net.URLEncoder
 import app.hyperpic.loader.Loader
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.rememberCoroutineScope
-import android.util.Log
-import app.hyperpic.utils.RequestManageExternalStoragePermission
+import app.hyperpic.utils.RequestStoragePermissions
 import androidx.compose.material3.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+
+import androidx.compose.runtime.LaunchedEffect
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HyperPic() {
-    RequestManageExternalStoragePermission()
-
     val context = LocalContext.current
     val navController = rememberNavController()
     val coroutineScope = rememberCoroutineScope()
     val loader = remember { Loader(context) }
     val snackbarHostState = remember { SnackbarHostState() }
+    
+    var hasPermissions by remember { mutableStateOf(false) }
 
-    Scaffold(
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
-    ) { paddingValues ->
-        NavHost(
-            navController = navController,
-            startDestination = "folder",
-            enterTransition = { EnterTransition.None },
-            exitTransition = { ExitTransition.None },
-        ) {
-            composable(route = "folder") {
-                Folder(
-                    onFolderClick = { folderId, folderName ->
-                        val encodedFolderName = URLEncoder.encode(folderName, "UTF-8")
-                        navController.navigate("folder_content/$folderId/$encodedFolderName")
+    if (!hasPermissions) {
+        RequestStoragePermissions(
+            onGranted = { hasPermissions = true },
+            onDenied = {}
+        )
+    } else {
+        Scaffold(
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+        ) { paddingValues ->
+            NavHost(
+                navController = navController,
+                startDestination = "folder",
+                enterTransition = { EnterTransition.None },
+                exitTransition = { ExitTransition.None },
+            ) {
+                composable(route = "folder") {
+                    Folder(
+                        onFolderClick = { folderId, folderName ->
+                            val encodedFolderName = URLEncoder.encode(folderName, "UTF-8")
+                            navController.navigate("folder_content/$folderId/$encodedFolderName")
+                        }
+                    )
+                }
+                composable(
+                    route = "folder_content/{folderId}/{folderName}",
+                    arguments = listOf(
+                        navArgument("folderId") { type = NavType.StringType },
+                        navArgument("folderName") { type = NavType.StringType }
+                    )
+                ) { backStackEntry ->
+                    val folderId = backStackEntry.arguments!!.getString("folderId")!!
+                    val folderName = backStackEntry.arguments!!.getString("folderName")!!
+                    FolderContent(
+                        folderId = folderId,
+                        folderName = folderName,
+                        onImageClick = { imageUri, imageName, imagePath ->
+                            val imageId = imageUri.lastPathSegment?.toLongOrNull() ?: 0L
+                            navController.navigate("image_view/$imageId/${URLEncoder.encode(imageName, "UTF-8")}")
+                        },
+                        onBack = { navController.popBackStack() }
+                    )
+                }
+                composable(
+                    route = "image_view/{imageId}/{imageName}",
+                    arguments = listOf(
+                        navArgument("imageId") { type = NavType.LongType },
+                        navArgument("imageName") { type = NavType.StringType }
+                    )
+                ) { backStackEntry ->
+                    val imageId = backStackEntry.arguments!!.getLong("imageId")
+                    val imageName = backStackEntry.arguments!!.getString("imageName")!!
+                    
+                    var imageData by remember { mutableStateOf<app.hyperpic.data.ImageData?>(null) }
+                    
+                    LaunchedEffect(imageId) {
+                        imageData = loader.getImageById(imageId)
                     }
-                )
-            }
-            composable(
-                route = "folder_content/{folderId}/{folderName}",
-                arguments = listOf(
-                    navArgument("folderId") { type = NavType.StringType },
-                    navArgument("folderName") { type = NavType.StringType }
-                )
-            ) { backStackEntry ->
-                val folderId = backStackEntry.arguments!!.getString("folderId")!!
-                val folderName = backStackEntry.arguments!!.getString("folderName")!!
-                FolderContent(
-                    folderId = folderId,
-                    folderName = folderName,
-                    onImageClick = { imageUri, imageName, imagePath ->
-                        val encodedImagePath = URLEncoder.encode(imagePath, "UTF-8")
-                        navController.navigate("image_view/${Uri.encode(imageUri.toString())}/${URLEncoder.encode(imageName, "UTF-8")}/$encodedImagePath")
-                    },
-                    onBack = { navController.popBackStack() }
-                )
-            }
-            composable(
-                route = "image_view/{imageUri}/{imageName}/{imagePath}",
-                arguments = listOf(
-                    navArgument("imageUri") { type = NavType.StringType},
-                    navArgument("imageName") { type = NavType.StringType },
-                    navArgument("imagePath") { type = NavType.StringType }
-                )
-            ) { backStackEntry ->
-                val imageUri = Uri.parse(
-                    Uri.decode(backStackEntry.arguments!!.getString("imageUri")!!)
-                )
-                val imageName = backStackEntry.arguments!!.getString("imageName")!!
-                val imagePath = Uri.decode(backStackEntry.arguments!!.getString("imagePath")!!)
-                ImageView(
-                    imageUri = imageUri,
-                    imageName = imageName,
-                    imagePath = imagePath,
-                    onBack = { navController.popBackStack() },
-                    onDelete = {
-                        coroutineScope.launch {
-                            val deleted = loader.deleteImageByPath(imagePath)
-                            if (deleted) {
-                            	navController.popBackStack()
-                                snackbarHostState.showSnackbar("Image deleted successfully.")
+                    
+                    imageData?.let { data ->
+                        ImageView(
+                            imageUri = data.uri,
+                            imageName = data.name,
+                            imagePath = data.path,
+                            onBack = { navController.popBackStack() },
+                            onDelete = {
+                                coroutineScope.launch {
+                                    val deleted = loader.deleteImageById(imageId)
+                                    if (deleted) {
+                                        navController.popBackStack()
+                                        snackbarHostState.showSnackbar("Image deleted successfully.")
+                                    } else {
+                                        snackbarHostState.showSnackbar("Failed to delete image.")
+                                    }
+                                }
                             }
+                        )
+                    } ?: run {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator()
                         }
                     }
-                )
+                }
             }
         }
     }
