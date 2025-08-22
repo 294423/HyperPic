779f273cc64c6cc48aa2665d65f383ccfdb5610a
294423 <riko64222@gmail.com>
2025-08-22 23:47:12 +0700

    Update VideoPlayer.kt

--- app/src/main/java/app/hyperpic/VideoPlayer.kt
+++ app/src/main/java/app/hyperpic/VideoPlayer.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -55,8 +54,8 @@ fun VideoPlayer(
             prepare()
         }
     }
-
-    var isPlaying by rememberSaveable { mutableStateOf(shouldPlay) }
+    
+    var isPlaying by remember { mutableStateOf(shouldPlay) }
 
     DisposableEffect(exoPlayer) {
         val listener = object : Player.Listener {
