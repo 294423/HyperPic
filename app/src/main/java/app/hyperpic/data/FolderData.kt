17e57bec14f2b3b873a908cf7583a15d2fe4d0d4
294423 <riko64222@gmail.com>
2025-08-22 22:48:53 +0700

    Update FolderData.kt

--- app/src/main/java/app/hyperpic/data/FolderData.kt
+++ app/src/main/java/app/hyperpic/data/FolderData.kt
@@ -2,20 +2,10 @@ package app.hyperpic.data
 
 import android.net.Uri
 
-/**
- * A data class to hold information about a photo folder.
- * The names have been simplified from the original.
- *
- * @property id The unique ID of the folder (MediaStore.Images.Media.BUCKET_ID).
- * @property name The display name of the folder.
- * @property count The number of images contained within this folder.
- * @property path The absolute path of the folder on the device.
- * @property thumbnailUri The content Uri of the most recent image, to be used as a thumbnail.
- */
 data class FolderData(
     val id: String,
     val name: String,
     val count: Int,
     val path: String,
-    val thumbnailUri: Uri
+    val thumbnailUris: List<Uri>
 )
