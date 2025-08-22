08964719598668477bedd4027a62d06d54618d75
294423 <riko64222@gmail.com>
2025-08-22 22:39:08 +0700

    Update MainActivity.kt

--- app/src/main/java/app/hyperpic/MainActivity.kt
+++ app/src/main/java/app/hyperpic/MainActivity.kt
@@ -3,28 +3,50 @@ package app.hyperpic
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import android.os.Build
 import android.os.Bundle
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.decode.VideoFrameDecoder
 
 class MainActivity : ComponentActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		enableEdgeToEdge()
-		super.onCreate(savedInstanceState)
-		setContent {
-			val darkTheme = isSystemInDarkTheme()
-            val colors = if (darkTheme) darkColorScheme() else lightColorScheme()
-            MaterialTheme(colorScheme = colors) {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    HyperPic()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            val context = LocalContext.current
+            val darkTheme = isSystemInDarkTheme()
+            
+            val colors = when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                    if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+                }
+                else -> {
+                    if (darkTheme) darkColorScheme() else lightColorScheme()
                 }
             }
-		}
+            
+            val videoEnabledLoader = ImageLoader.Builder(LocalContext.current)
+                .components {
+                    add(VideoFrameDecoder.Factory())
+                }
+                .build()
+
+            MaterialTheme(colorScheme = colors) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    HyperPic(videoEnabledLoader)
+                }
+            }
+        }
     }
 }
