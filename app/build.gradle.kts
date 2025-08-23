ff797d55e226799b91cdd1a7c0a6d79f76cb9f86
294423 <riko64222@gmail.com>
2025-08-23 19:44:50 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.ui:ui:1.8.3")
-    implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui:1.10.0-alpha01")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha02")
+    implementation("androidx.activity:activity-compose:1.12.0-alpha06")
     implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("io.coil-kt:coil-video:2.7.0")
